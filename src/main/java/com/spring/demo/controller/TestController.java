15b9bd2e0ef72c18b80026cd5571450ad5f701f0
TestGitProjects <testinggitprojects@gmail.com>
2025-11-22 22:53:37 +0530

    properties support

--- src/main/java/com/spring/demo/controller/TestController.java
+++ src/main/java/com/spring/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.spring.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class TestController {
 
+    @Value("${dynamic.hello:defaultDynamicHello}")
+    private String helloMessage;
+
     @GetMapping("/test")
     public String test() {
-        return "Hello!";
+        return helloMessage;
     }
 }
